7e76e319fdc366db093e942f8907df03a8c2ea73
miguel-jr96 <masjr19@gmail.com>
2025-02-11 10:58:25 -0300

    Update tes.py

--- topi/Paginas/tes.py
+++ topi/Paginas/tes.py
@@ -50,7 +50,7 @@ class ResumoArtigo(BaseModel):
 
 # Função para definir o modelo do gpt usado e para obter o resumo
 def obter_resumo_artigo(texto):
-    completacao = client.chat.completions.create(
+    completacao = client.beta.chat.completions.parse(
         model='gpt-4o-mini',
         messages=[
             {"role": "system", "content": prompt},
